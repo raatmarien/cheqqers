69b97da3c4c9164aecc0db1e9a3e47560d77966f
LuukvandenNouweland <70901222+LuukvandenNouweland@users.noreply.github.com>
2024-04-08 16:23:27 +0200

    reverted to not printing cirquit because it doesnt work correctly and it may break everything but atleats it is archived in the commits

--- quantum_checkers.py
+++ quantum_checkers.py
@@ -462,7 +462,6 @@ class Checkers:
         self.player = CheckersPlayer.BLACK if self.player == CheckersPlayer.WHITE else CheckersPlayer.WHITE
         self.legal_moves = self.calculate_possible_moves(self.player)
         self.status = self.result()
-        print(self.board.circuit)
 
     def get_board(self) -> str:
         """Returns the Checkers board in ASCII form. Also returns dictionary with id as key.
@@ -812,9 +811,9 @@ class Checkers:
             raise ValueError("No second target given")
         original_piece = self.classical_squares[str(move.source_id)]
         if(not self.SIMULATE_QUANTUM):
-            # CheckersSplit(CheckersSquare.FULL, self.rules)(self.squares[str(move.source_id)], self.squares[str(move.target1_id)], self.squares[str(move.target2_id)])
-            split = alpha.Split()
-            split(self.squares[str(move.source_id)], self.squares[str(move.target1_id)], self.squares[str(move.target2_id)])
+            CheckersSplit(CheckersSquare.FULL, self.rules)(self.squares[str(move.source_id)], self.squares[str(move.target1_id)], self.squares[str(move.target2_id)])
+            # split = alpha.Split()
+            # split(self.squares[str(move.source_id)], self.squares[str(move.target1_id)], self.squares[str(move.target2_id)])
         self.classical_squares[str(move.target1_id)] = Piece(id=str(move.target1_id), color=original_piece.color, king=original_piece.king, superposition=True)
         self.classical_squares[str(move.target2_id)] = Piece(id=str(move.target2_id), color=original_piece.color, king=original_piece.king, superposition=True)
 
