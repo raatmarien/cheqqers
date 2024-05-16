b5a2b4a806a50cb19b40dd4d2a0ca904fdb1fde6
LuukvandenNouweland <70901222+LuukvandenNouweland@users.noreply.github.com>
2024-05-16 14:54:41 +0200

    made (untested) heuristic function

--- players.py
+++ players.py
@@ -1,6 +1,7 @@
 from random import randint
 import traceback
 from mcts import MCTS
+from enums import CheckersPlayer
 
 class bot():
     def __init__(self) -> None:
@@ -38,11 +39,42 @@ class random_bot(bot):
             print(traceback.format_exc())
             print(possible_moves)
     
-# class heurisitc_bot():
-#     def select_move(self, game, possible_moves):
-#         for i in possible_moves:
-#             game.get_copy()
-#     def evaluate_board(self, game)
+class heurisitc_bot():
+    def __init__(self, game, depth=2) -> None:
+        self.game = game
+        self.depth = depth
+        self.player = game.player
+
+    def select_move(self, game, parent_player: CheckersPlayer, curr_depth=0):
+        scores = []
+        possible_moves = self.game.legal_moves
+        if(curr_depth == self.depth): # if we reached the depth we want to go to, evaluate the board
+            score =  self.evaluate_board(game)
+            if(parent_player != game.player): # Invert the score if the player is not the same as the parent player
+                score = -score
+            return score
+        for i in possible_moves: # For all moves we can do from this position, do the move and recursively call this function
+            cp = game.get_copy()
+            player = cp.player
+            cp.move(i) # player probably changes here
+            scores.append(self.select_move(cp, player, curr_depth+1)) # is gonna return a score for this specific move
+        if(curr_depth == 0):
+            return possible_moves[scores.index(max(scores))]
+        return sum(scores)/len(scores) # Return average of scores
+
+    def evaluate_board(self, game):
+        score = 0
+        modifier = 1
+        if(self.player == CheckersPlayer.BLACK):
+            modifier = -1
+        for key, value in self.classical_squares.items():
+            if(value.color == CheckersPlayer.WHITE):
+                score += self.chance
+            elif(value.color == CheckersPlayer.BLACK):
+                score -= self.chance
+        score = score * modifier # Multiply by modifier to make sure the score is correct for the player
+        return score
+        pass
            
     
             
