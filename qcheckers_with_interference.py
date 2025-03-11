a7048605dca3017e41f047c0c07bc21b84368987
Marien Raat <mail@marienraat.nl>
2025-03-11 18:00:45 +0100

    Get classical checkers working and verify results with old code

--- qcheckers_with_interference.py
+++ qcheckers_with_interference.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import random
+import statistics
 
 
 class ClassicalSquareState(Enum):
@@ -11,6 +13,12 @@ class PieceColor(Enum):
     WHITE = 0
     BLACK = 1
 
+    def other(self):
+        if self == PieceColor.WHITE:
+            return PieceColor.BLACK
+        else:
+            return PieceColor.WHITE
+
 
 class Piece:
     color: PieceColor
@@ -22,6 +30,11 @@ class Piece:
 
 
 class Move:
+    is_take_move: bool
+
+    def __init__(self, is_take_move):
+        self.is_take_move = is_take_move
+
     def print_move(self):
         return "TODO"
 
@@ -30,7 +43,8 @@ class ClassicalMove(Move):
     from_index: int
     to_index: int
 
-    def __init__(self, from_index, to_index):
+    def __init__(self, is_take_move, from_index, to_index):
+        super().__init__(is_take_move)
         self.from_index = from_index
         self.to_index = to_index
 
@@ -40,7 +54,8 @@ class SplitMove(Move):
     to_index1: int
     to_index2: int
 
-    def __init__(self, from_index, to_index1, to_index2):
+    def __init__(self, is_take_move, from_index, to_index1, to_index2):
+        super().__init__(is_take_move)
         self.from_index = from_index
         self.to_index1 = to_index1
         self.to_index2 = to_index2
@@ -51,7 +66,8 @@ class MergeMove(Move):
     from_index2: int
     to_index: int
 
-    def __init__(self, from_index1, from_index2, to_index):
+    def __init__(self, is_take_move, from_index1, from_index2, to_index):
+        super().__init__(is_take_move)
         self.from_index1 = from_index1
         self.from_index2 = from_index2
         self.to_index = to_index
@@ -137,11 +153,11 @@ class Board:
                         i_to = self.xy_index_map[(x_to, y_to)]
                         if self.classic_occupancy[i_to] != ClassicalSquareState.EMPTY:
                             continue
-                        moves.append(ClassicalMove(i, i_to))
+                        moves.append(ClassicalMove(True, i, i_to))
                     else:
                         if self.classic_occupancy[i_] != ClassicalSquareState.EMPTY:
                             continue
-                        moves.append(ClassicalMove(i, i_))
+                        moves.append(ClassicalMove(False, i, i_))
 
         # Note: We don't allow split or merge moves with takes
         if not take:
@@ -165,7 +181,8 @@ class Board:
             for i in range(len(related_moves)):
                 for j in range(i + 1, len(related_moves)):
                     split_moves.append(
-                        SplitMove(move.from_index,
+                        SplitMove(False,
+                                  move.from_index,
                                   related_moves[i].to_index,
                                   related_moves[j].to_index))
 
@@ -185,7 +202,8 @@ class Board:
             for i in range(len(related_moves)):
                 for j in range(i + 1, len(related_moves)):
                     merge_moves.append(
-                        MergeMove(related_moves[i].from_index,
+                        MergeMove(False,
+                                  related_moves[i].from_index,
                                   related_moves[j].from_index,
                                   move.to_index))
 
@@ -195,6 +213,26 @@ class Board:
         if index1 == index2:
             return True
         return True  # TODO actually check
+
+    def display(self):
+        """Display the current board state"""
+        board_display = ""
+        for y in range(self.size):
+            row = ""
+            for x in range(self.size):
+                if (x, y) not in self.xy_index_map:
+                    row += "□ "  # White square
+                    continue
+                
+                i = self.xy_index_map[(x, y)]
+                if self.classic_occupancy[i] == ClassicalSquareState.EMPTY:
+                    row += "· "  # Empty black square
+                elif self.piece_map[i].color == PieceColor.WHITE:
+                    row += "○ " if not self.piece_map[i].crowned else "♔ "  # White piece
+                else:
+                    row += "● " if not self.piece_map[i].crowned else "♚ "  # Black piece
+            board_display += row + "\n"
+        return board_display
 
 
 class GameState(Enum):
@@ -207,11 +245,82 @@ class GameState(Enum):
 class Game:
     board: Board
     moves: list[Move]
-    state: GameState
-    white_turn: bool
+    turn: PieceColor
+    moves_since_take: int
 
     def __init__(self, size, start_rows):
         self.board = Board(size, start_rows)
         self.moves = []
-        self.state = GameState.IN_PROGRESS
-        self.white_turn = True
+        self.turn = PieceColor.WHITE
+        self.moves_since_take = 0
+
+    def get_game_state(self) -> GameState:
+        if self.moves_since_take >= 40:
+            return GameState.DRAW
+
+        moves = self.board.get_possible_moves(self.turn)
+        if len(moves) == 0:
+            return GameState.WHITE_WON if self.turn == PieceColor.BLACK\
+                else GameState.BLACK_WON
+
+        return GameState.IN_PROGRESS
+
+    def apply_move(self, move: Move):
+        self.moves_since_take += 1
+        if isinstance(move, ClassicalMove):
+            self._apply_classical_move(move)
+        elif isinstance(move, SplitMove):
+            self._apply_split_move(move)
+        elif isinstance(move, MergeMove):
+            self._apply_merge_move(move)
+
+        if move.is_take_move:
+            self.moves_since_take = 0
+
+        if not move.is_take_move or\
+           len(self.board.get_take_moves(self.turn)) == 0:
+            self.turn = self.turn.other()
+
+    def _apply_classical_move(self, move: ClassicalMove):
+        piece = self.board.piece_map[move.from_index]
+
+        if move.is_take_move:
+            # Calculate the position of the taken piece
+            from_x, from_y = self.board.index_xy_map[move.from_index]
+            to_x, to_y = self.board.index_xy_map[move.to_index]
+
+            # The taken piece is in between the from and to positions
+            taken_x = (from_x + to_x) // 2
+            taken_y = (from_y + to_y) // 2
+            taken_index = self.board.xy_index_map[(taken_x, taken_y)]
+
+            # Remove the taken piece
+            self.board.piece_map[taken_index] = None
+            self.board.classic_occupancy[taken_index] = ClassicalSquareState.EMPTY
+
+        # Move the piece to the new position
+        self.board.piece_map[move.to_index] = piece
+        self.board.classic_occupancy[move.to_index] = ClassicalSquareState.OCCUPIED
+
+        # Clear the original position
+        self.board.piece_map[move.from_index] = None
+        self.board.classic_occupancy[move.from_index] = ClassicalSquareState.EMPTY
+
+        # Check if the piece should be crowned (reached the opposite edge)
+        if not piece.crowned:
+            to_x, to_y = self.board.index_xy_map[move.to_index]
+            if (piece.color == PieceColor.WHITE and to_y == self.board.size - 1) or \
+               (piece.color == PieceColor.BLACK and to_y == 0):
+                piece.crowned = True
+
+        # Add the move to the history
+        self.moves.append(move)
+
+    def _apply_split_move(self, move: SplitMove):
+        # Implement split move logic here
+        pass
+
+    def _apply_merge_move(self, move: MergeMove):
+        # Implement merge move logic here
+        pass
+
--- test_qcheckers_with_interference.py
+++ test_qcheckers_with_interference.py
@@ -1,6 +1,8 @@
 import unittest
 from enum import Enum
 from qcheckers_with_interference import Board, Piece, PieceColor, ClassicalSquareState, Game, GameState, Move, SplitMove, MergeMove, ClassicalMove
+import statistics
+import random
 
 
 class TestQuantumCheckers(unittest.TestCase):
@@ -84,10 +86,10 @@ class TestQuantumCheckers(unittest.TestCase):
         game = Game(board_size, start_rows)
         
         # Check that the game starts with white's turn
-        self.assertTrue(game.white_turn)
+        self.assertTrue(game.turn, PieceColor.WHITE)
         
         # Check that the game starts with IN_PROGRESS state
-        self.assertEqual(game.state, GameState.IN_PROGRESS)
+        self.assertEqual(game.get_game_state(), GameState.IN_PROGRESS)
         
         # Check that no moves have been made yet
         self.assertEqual(len(game.moves), 0)
@@ -329,6 +331,50 @@ class TestTakeMoves(unittest.TestCase):
         self.assertEqual(all_move_set, take_move_set, 
                         "get_possible_moves should return the exact same moves as get_take_moves")
 
+
+class TestClassicalGame(unittest.TestCase):
+    def test_random_classical_game(self, board_size=8, start_rows=1):
+        """Run a random game of quantum checkers with only classical moves"""
+        amount = 1000
+        moves = []
+        white_won = 0
+        black_won = 0
+        for _ in range(amount):
+            game = Game(board_size, start_rows)
+            move_count = 0
+            
+            while game.get_game_state() == GameState.IN_PROGRESS:
+                possible_moves = game.board.get_possible_moves(game.turn)
+                
+                # Filter for only classical moves for this test
+                classical_moves = [move for move in possible_moves
+                                   if isinstance(move, ClassicalMove)]
+                if not classical_moves:
+                    break
+                
+                # Select a random move
+                random_move = random.choice(classical_moves)
+                
+                # Apply the move
+                prev_turn = game.turn
+                game.apply_move(random_move)
+                if game.turn != prev_turn:
+                    move_count += 1
+                
+            moves.append(move_count)
+            result = game.get_game_state()
+            if result == GameState.WHITE_WON:
+                white_won += 1
+            elif result == GameState.BLACK_WON:
+                black_won += 1
+        
+        self.assertGreater(statistics.mean(moves), 40)
+        self.assertLess(statistics.mean(moves), 55)
+        self.assertGreater(white_won, 350)
+        self.assertLess(white_won, 500)
+        self.assertGreater(black_won, 350)
+        self.assertLess(black_won, 500)
+    
 
 
 if __name__ == '__main__':
