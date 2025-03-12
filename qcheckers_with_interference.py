bd7a3cb004149a7437d31e7bbea16d06d2b56c8f
Marien Raat <mail@marienraat.nl>
2025-03-12 01:18:02 +0100

    Fix draw rate

--- qcheckers_with_interference.py
+++ qcheckers_with_interference.py
@@ -433,7 +433,7 @@ class Game:
                 self.entanglements.append(
                     PieceEntanglement(superposition_taken, superposition_from))
 
-                return
+                return True
             else:
                 is_there = self.measure(taken_index)
                 if not is_there:
